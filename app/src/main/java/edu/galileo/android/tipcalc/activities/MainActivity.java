6920d8b5434062bc5cba57267f6f14615c100ce0
xue2sheng <andres.sevillano.vallejo@gmail.com>
2016-06-15 15:00:00 +0100

    Refactored code

--- app/src/main/java/edu/galileo/android/tipcalc/activities/MainActivity.java
+++ app/src/main/java/edu/galileo/android/tipcalc/activities/MainActivity.java
@@ -1,15 +1,53 @@
 package edu.galileo.android.tipcalc.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import edu.galileo.android.tipcalc.R;
+import edu.galileo.android.tipcalc.fragments.TipHistoryListFragment;
+import edu.galileo.android.tipcalc.fragments.TipHistoryListFragmentListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TipHistoryListFragmentListener fragmentListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /*
+        TipHistoryListFragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragmentList);
+        fragment.setRetainInstance(true);
+        fragmentListener = (TipHistoryListFragmentListener) fragment;
+        */
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_about) {
+            about();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void about() {
+        TipCalcApp app = (TipCalcApp) getApplication();
+        String strUrl = app.getAboutUrl();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(strUrl));
+        startActivity(intent);
     }
 }
